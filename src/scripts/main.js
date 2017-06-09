e2d16d760352977533d72f79c2f1f90df21d6a8c
Py151112327 <Pyterwahyu@gmail.com>
2017-06-10 01:36:07 +0700

    penambahan javascript calculator

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,18 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
-  keys = require('bespoke-keys'),
-  touch = require('bespoke-touch'),
-  bullets = require('bespoke-bullets'),
-  backdrop = require('bespoke-backdrop'),
-  scale = require('bespoke-scale'),
-  hash = require('bespoke-hash'),
-  progress = require('bespoke-progress'),
-  forms = require('bespoke-forms');
+    keys = require('bespoke-keys'),
+    touch = require('bespoke-touch'),
+    bullets = require('bespoke-bullets'),
+    backdrop = require('bespoke-backdrop'),
+    scale = require('bespoke-scale'),
+    hash = require('bespoke-hash'),
+    progress = require('bespoke-progress'),
+    forms = require('bespoke-forms'),
+    nebula = require('bespoke-theme-nebula');
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  nebula(),  
   keys(),
   touch(),
   bullets('li, .bullet'),
@@ -22,9 +22,7 @@ bespoke.from('article', [
   progress(),
   forms()
 ]);
-
 // Prism syntax highlighting
 // This is actually loaded from "bower_components" thanks to
 // debowerify: https://github.com/eugeneware/debowerify
 require('prism');
-
